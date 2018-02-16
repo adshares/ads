e299e4fa18892464092a3582f07ea86b2a949f16
sylwek <s.raczynski@bioinfo.com>
2018-02-16 09:14:32 +0100

    install two nodes.

--- test/functional/installesc.sh
+++ test/functional/installesc.sh
@@ -3,11 +3,13 @@
 
 function prepareNode
 {
+    echo '.............................prepareNode'$1
+
         mkdir $nodename
 	ln -s $current/escd $nodename/escd
 
-	cd $nodename
-	echo 'svid='$1 > options.cfg
+        cd $nodename
+        echo 'svid='$1 > options.cfg
 	echo 'offi='$2 >> options.cfg
 	echo 'port='$3 >> options.cfg
 	echo 'addr=127.0.0.1' >> options.cfg
@@ -28,11 +30,15 @@ function prepareNode
 
 function prepareClientKeys
 {
+    echo '.............................prepareClientKeys'$1
+
     cd $1
 
     if [ $2 == 1 ]
     then
     {
+        echo 'host=127.0.0.1'> settings.cfg
+        echo 'port=9091' >> settings.cfg
         echo 'address=0001-00000000-XXXX' >> settings.cfg
         echo 'secret=14B183205CA661F589AD83809952A692DFA48F5D490B10FD120DA7BF10F2F4A0' >> settings.cfg
     }
@@ -41,6 +47,8 @@ function prepareClientKeys
     if [ $2 == 2 ]
     then
     {
+        echo 'host=127.0.0.1' > settings.cfg
+        echo 'port=9091' >> settings.cfg
         echo 'address=0001-00000001-XXXX' >> settings.cfg
         echo 'secret=5BF11F5D0130EC994F04B6C5321566A853B7393C33F12E162A6D765ADCCCB45C' >> settings.cfg
     }
@@ -49,7 +57,9 @@ function prepareClientKeys
     if [ $2 == 3 ]
     then
     {
-        echo 'address=0001-00000002-XXXX' >> settings.cfg
+        echo 'host=127.0.0.1' > settings.cfg
+        echo 'port=9092' >> settings.cfg
+        echo 'address=0002-00000000-XXXX' >> settings.cfg
         echo 'secret=E4882F10389E14D7E55050E2D05EF50701A8A5FD8D9AE188D9AE8F377E410254' >> settings.cfg
     }
     fi
@@ -58,20 +68,22 @@ function prepareClientKeys
 
 function initFirstNode
 {
-    echo 'server starting'
+    echo '.............................initFirstNode\n'
 
     cd 'node1'
     nohup ./escd --init 1 > nodeout.txt &
     ##./escd --init 1 > nodeout.txt &
 
     cd ..
-    sleep 40
+    sleep 1m
 
     echo 'INITALIZATION NODE 1 FINISHED'
 }
 
 function prepareClient
 {
+        echo '.............................prepareClient'$1
+
         username='user'$1
 	mkdir $username 
 	ln -s $current/esc $username/esc
@@ -83,25 +95,33 @@ function prepareClient
 
 function setUpUser1
 {
+    echo '.............................setUpUser1'
+
     cd 'user1'
 
     echo '{"run":"get_me"}' | ./esc
+
+    echo '.............................change_account_key for User 1'
+
     (echo '{"run":"get_me"}'; echo '{"run":"change_account_key","pkey":"D69BCCF69C2D0F6CED025A05FA7F3BA687D1603AC1C8D9752209AC2BBF2C4D17","signature":"7A1CA8AF3246222C2E06D2ADE525A693FD81A2683B8A8788C32B7763DF6037A5DF3105B92FEF398AF1CDE0B92F18FE68DEF301E4BF7DB0ABC0AEA6BE24969006"}') | ./esc
 
     sleep 1m
 
-    rm settings.cfg
+    echo 'host=127.0.0.1'> settings.cfg
+    echo 'port=9091' >> settings.cfg
     echo 'address=0001-00000000-9B6F' >> settings.cfg
-    echo 'secret= FF767FC8FAF9CFA8D2C3BD193663E8B8CAC85005AD56E085FAB179B52BD88DD6' >> settings.cfg
+    echo 'secret=FF767FC8FAF9CFA8D2C3BD193663E8B8CAC85005AD56E085FAB179B52BD88DD6' >> settings.cfg
 
     echo '{"run":"get_me"}' | ./esc
     cd ..
 
-    sleep 1m
+    sleep 25
 }
 
 function createAccountForUser2
 {
+    echo '.............................createAccountForUser2'
+
     cd 'user1'
 
     echo '----------------------------------'
@@ -127,10 +147,9 @@ function createAccountForUser2
 
 function changeKeysforUser2
 {
-    echo 'changeKeysforUser2 -----------'
+    echo '.............................changeKeysforUser2'
 
     cd 'user1'
-    ##change pkey for user 2
 
     (echo '{"run":"get_me"}'; echo '{"run":"change_account_key","pkey":"C9965A1417F52B22514559B7608E4E2C1238FCA3602382C535D42D1759A2F196","signature":"ED8479C0EDA3BB02B5B355E05F66F8161811F5AD9AE9473AA91E2DA32457EAB850BC6A04D6D4D5DDFAB4B192D2516D266A38CEA4251B16ABA1DF1B91558A4A05"}' )  | ./esc  --address 0001-00000001-8B4E
 
@@ -138,12 +157,11 @@ function changeKeysforUser2
 
     cd 'user2'
 
-    rm settings.cfg
-    echo 'address=0001-00000001-8B4E ' >> settings.cfg
+    echo 'address=0001-00000001-8B4E ' > settings.cfg
     echo 'secret= 5BF11F5D0130EC994F04B6C5321566A853B7393C33F12E162A6D765ADCCCB45C ' >> settings.cfg
     chmod go-r settings.cfg
 
-    sleep 25
+    sleep 1m
 
     echo '{"run":"get_me"}' | ./esc
 
@@ -153,17 +171,13 @@ function changeKeysforUser2
 
 function sendCash
 {
-    echo 'sendcash -----------'
-    echo '{"run":"send_one","address":"'$1'","amount":'$2',"message":"000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F"}'
+    echo '.............................sendCash'
 
     cd 'user1'
-
-    #(echo '{"run":"get_me"}'; echo '{"run":"send_one","address":"0001-00000001-8B4E","amount":70.0,"message":"000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F"}') | ./esc
 
     (echo '{"run":"get_me"}'; echo '{"run":"send_one","address":"'$1'","amount":'$2',"message":"000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F"}') | ./esc
 
     cd ..
-
 
     sleep 25
 }
@@ -193,7 +207,7 @@ do
 	echo $nodename	
         let "officeport = $officeport + 1"
         let "serverport = $serverport + 1"
-        let "peerport = $serverport-1"
+        let "peerport   = $serverport-1"
 
 	prepareNode $i $officeport $serverport $peerport
 	prepareClient $i        	
@@ -207,7 +221,7 @@ sendCash "0001-00000001-8B4E" 70.0
 
 
 
-for i in {1..100}
+for i in {1..3}
 do
     sendCash "0001-00000001-8B4E" 70.0
 
@@ -224,12 +238,37 @@ done
 
     cd ..
 
-##sleep 1m
+sleep 1m
 
 cd 'node2'
 
+echo 'FF767FC8FAF9CFA8D2C3BD193663E8B8CAC85005AD56E085FAB179B52BD88DD6' > key/key.txt
+cd ..
+
+cd 'user1'
+
+echo '.............................change_node_key'
+
+(echo '{"run":"get_me"}'; echo '{"run":"change_node_key","pkey":"D69BCCF69C2D0F6CED025A05FA7F3BA687D1603AC1C8D9752209AC2BBF2C4D17","node":"2"}') | ./esc
+
+cd ..
+
+sleep 2m
+
+cd 'node2'
+
+for file in $(find ../node1/blk -name servers.srv | sort -rn)
+do
+cp ../node1/$file ./
+echo $file
+break;
+done
 
 
+echo '.............................nohup node2'
+
+nohup ./escd -m 1 -f 1 > nodeout.txt &
+cd ..
 
 echo 'server started'
 
