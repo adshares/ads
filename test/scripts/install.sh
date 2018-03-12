2d2b35e216f2b4cff6415ea2327d08287098fdb6
Krzysztof Kuchta <kkuchta@togetherdata.com>
2018-03-12 17:35:13 +0100

    installing install scripts.

--- test/scripts/install.sh
+++ test/scripts/install.sh
@@ -1,9 +1,8 @@
 mkdir -p /builds/proxy/build
 cd /builds/proxy/build
-cmake -DCMAKE_INSTALL_PREFIX=/usr/local/bin -DCMAKE_BUILD_TYPE=Debug /builds/proxy/hpx/src
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Debug /builds/proxy/hpx/src
 make clean
 make -j4
 sudo make install
 echo $PATH
 ls /usr/local/bin
-tail -f -n 40 /dev/null
