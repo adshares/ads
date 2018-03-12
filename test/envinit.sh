2a86ff8e01c6ebb1b82c8112948a5daac43c9c7e
Krzysztof Kuchta <kkuchta@togetherdata.com>
2018-03-12 16:27:33 +0100

    Fixing envinit.sh

--- test/envinit.sh
+++ test/envinit.sh
@@ -1,1 +1,6 @@
-pwd
+mkdir -p /builds/proxy/build
+cd /builds/proxy/build
+cmake -DCMAKE_INSTALL_PREFIX=./  -DEXECUTE_LOCAL_TEST=True -DCMAKE_BUILD_TYPE=Debug /builds/proxy/hpx/src
+make clean
+make -j4
+make install
