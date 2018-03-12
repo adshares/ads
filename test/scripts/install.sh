fe167306987fe83cbbda770d03b33be1a9298bff
Krzysztof Kuchta <kkuchta@togetherdata.com>
2018-03-12 16:56:07 +0100

    Adding sudo to install.

--- test/scripts/install.sh
+++ test/scripts/install.sh
@@ -3,6 +3,5 @@ cd /builds/proxy/build
 cmake -DCMAKE_INSTALL_PREFIX=./ -DCMAKE_BUILD_TYPE=Debug /builds/proxy/hpx/src
 make clean
 make -j4
-make install
-which escd
-which esc
+sudo make install
+esc
