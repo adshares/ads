c7bfd3bb602f7027bcb79d75cdfd25e5e19e61ca
Krzysztof Kuchta <kkuchta@togetherdata.com>
2018-03-12 15:38:30 +0100

    Fixing build.

--- scripts/buildandrunDebug.sh
+++ scripts/buildandrunDebug.sh
@@ -1,12 +1,9 @@
 cd ..
-pwd
-ls
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=./  -DEXECUTE_LOCAL_TEST=True -DCMAKE_BUILD_TYPE=Debug ../src
 make clean
 make -j4
 make install
-pwd
-cd bin
+cd ../scripts
 ./installesc.sh 7290 8290 8290 "TRUE"
