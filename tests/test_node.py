216fab9670a1d5213ddb21b0e91b2262bd0def10
Krzysztof Kuchta <kkuchta@togetherdata.com>
2018-03-14 12:06:52 +0100

    Fixing test_node standart output.

--- tests/test_node.py
+++ tests/test_node.py
@@ -2,38 +2,26 @@ from . import clean_node_dir, create_node_env, get_node_path_dir
 from . import INIT_NODE_ID,  ESCD_BIN_PATH
 
 import subprocess
-import os
-import time
 
 
-def test_first_node_start(gen_blocks_count=4):
+def test_first_node_start(gen_blocks_count=2):
     clean_node_dir(INIT_NODE_ID)
     create_node_env(INIT_NODE_ID, 8000, 8001, 8002)
     node_dir = get_node_path_dir(INIT_NODE_ID)
 
-    log_fd = os.open(os.path.join(node_dir, "log.txt"), os.O_RDWR | os.O_CREAT)
-
-    process = subprocess.Popen(["stdbuf", "-oL", ESCD_BIN_PATH, "--init", "1"],
-                               cwd=node_dir, bufsize=1, stdout=log_fd)
+    process = subprocess.Popen([ESCD_BIN_PATH, "--init", "1"],
+                               cwd=node_dir, bufsize=1, stdout=subprocess.PIPE)
 
     # Check in log if blocks are created
     blocks_counter = 0
-    with open(os.path.join(node_dir, "log.txt"), 'r') as handler:
+    for line in process.stdout:
+        print(line, blocks_counter)
+        if line == b"NEW BLOCK created\n":
+            blocks_counter += 1
 
-        while True:
-            line = handler.readline()
-            if line == "NEW BLOCK created\n":
-                print(line)
+        if blocks_counter == gen_blocks_count:
+            break
 
-                blocks_counter += 1
-
-            if blocks_counter == gen_blocks_count:
-                break
-
-            if not line:
-                time.sleep(0.5)
-
-    os.close(log_fd)
     process.terminate()
     process.kill()
 
