e5310e613ac9804d626f40f8c1061611de86d996
denis rozlomiy <d.rozlomiy@bioinfo.pl>
2018-04-11 10:20:39 +0200

    deleted extras charts

--- tests/test_node.py
+++ tests/test_node.py
@@ -23,8 +23,7 @@ def test_node_create_node(init_node_process, node_id="2"):
 
     response = exec_esc_cmd(INIT_CLIENT_ID, {"run": "get_block"})
 
-
-    assert response['block']['nodes'][-1]['id'] == '0002'cd
+    assert response['block']['nodes'][-1]['id'] == '0002'
 
     NEW_PKEY = "D69BCCF69C2D0F6CED025A05FA7F3BA687D1603AC1C8D9752209AC2BBF2C4D17"
     NEW_PRIV_KEY = "FF767FC8FAF9CFA8D2C3BD193663E8B8CAC85005AD56E085FAB179B52BD88DD6"
