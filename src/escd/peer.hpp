c15ebb8fa40dd65dcd32412e6e5ca0711ac426f0
sylwek <s.raczynski@bioinfo.com>
2018-05-09 14:54:53 +0200

    #69 - Build error plus proper return from authenticate.

--- src/escd/peer.hpp
+++ src/escd/peer.hpp
@@ -1285,7 +1285,7 @@ NEXTUSER:
             ELOG("Fast sync VIPHASH: %.*s\n", 2*SHA256_DIGEST_LENGTH, hash_text);
             if(strncmp(hash_text, opts_.viphash.c_str(), 2*SHA256_DIGEST_LENGTH)) {
                 ELOG("Synced to invalid VIPHASH, expected %.*s\n", 2*SHA256_DIGEST_LENGTH, opts_.viphash.c_str());
-                throw std::runtime_error("Synced to invalid VIPHASH");
+                return 0;
             }
         }
 
