4982759a2826fc3a698ee282c07d3e36428be985
Krzysztof Kuchta <kkuchta@togetherdata.com>
2018-05-16 20:04:11 +0200

    Fixing.

--- tests/scripts/install.sh
+++ tests/scripts/install.sh
@@ -1,5 +1,5 @@
 mkdir -p /builds/proxy/hpx/build
 cd /builds/proxy/hpx/build
-cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Debug /builds/proxy/hpx/src
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_PROJECT_CONFIG=esc -DCMAKE_BUILD_TYPE=Debug /builds/proxy/hpx/src
 make clean
 make -j4
