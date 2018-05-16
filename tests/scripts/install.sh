0c709554ae661fb1117fa9da02f7620888a16b1e
Krzysztof Kuchta <kkuchta@togetherdata.com>
2018-05-16 20:07:52 +0200

    Cleaning.

--- tests/scripts/install.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p /builds/proxy/hpx/build
-cd /builds/proxy/hpx/build
-cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_PROJECT_CONFIG=esc -DCMAKE_BUILD_TYPE=Debug /builds/proxy/hpx/src
-make clean
-make -j4
