bb4738c30eabf09031f1d99f6a5fe76ab6f1a8a0
Michal Siek <m.siek@bioinfo.pl>
2018-05-24 15:42:10 +0200

    #102 added case in process_message function

--- src/common/command/broadcastmsg.h
+++ src/common/command/broadcastmsg.h
@@ -1,8 +1,6 @@
 #ifndef BROADCASTMSG_H
 #define BROADCASTMSG_H
 
-#define MAX_BROADCAST_LENGTH  32000 //64k in hex multiplied by 2
-
 #include "abstraction/interfaces.h"
 #include "command/pods.h"
 #include "default.hpp"
