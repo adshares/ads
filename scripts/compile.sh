56ba98e039ddb245f1273eaaa0ef5b6e1d278b3c
Adam Włodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-06-01 15:43:39 +0200

    One-step compilation/dependency installation on linux.

---
+++ scripts/compile.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env sh
+
+install_dependencies() {
+
+    command -v apt-get
+    if [ $? -eq 0 ]; then
+        install_apt_dependencies $1
+        return 0
+    fi
+
+    return 1
+
+    # Following code in unsupported.
+
+    command -v yum
+    if [ $? -eq 0 ]; then
+        install_yum_dependencies $1
+        return 0
+    fi
+
+    command -v apk
+    if [ $? -eq 0 ]; then
+        install_apk_dependencies $1
+        return 0
+    fi
+
+    return 1
+}
+
+install_apt_dependencies() {
+
+    apt-get update -y
+    apt-get install -y openssl libboost-all-dev
+
+    if [ $1 ]; then
+        apt-get -y install libssl-dev
+        apt-get -y install build-essential make cmake
+    fi
+}
+
+# Unsupported
+install_yum_dependencies() {
+
+    yum check-update
+    yum install -y openssl boost-devel.x86_64
+
+    if [ $1 ]; then
+        yum install -y gcc72.x86_64 gcc72-c++.x86_64
+        yum install -y openssl-devel.x86_64
+        yum install -y make cmake
+    fi
+}
+
+# Unsupported
+install_apk_dependencies() {
+
+    apk update
+    apk add openssl
+    apk add boost-dev
+
+    if [ $1 ]; then
+        apk add openssl-dev
+        apk add gcc g++
+        apk add make cmake
+    fi
+}
+
+compile() {
+    if [ $1 ]; then
+        mkdir /esc
+        cd /esc
+        cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PROJECT_CONFIG=esc /esc_repo/src
+        make -j `nproc` escd esc
+    fi
+}
+
+# Main function
+
+# Say 'please' to compile, otherwise it will only install depdencdencies
+
+install_dependencies $1
+if [ $? -eq 0 ]; then
+    compile $1
+fi
