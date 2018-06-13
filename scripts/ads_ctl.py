534efafd74baedc6b81a58ba40dcef510ab665bb
Adam Włodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-06-13 13:10:13 +0200

    Refactoring

--- scripts/ads_ctl.py
+++ scripts/ads_ctl.py
@@ -11,7 +11,6 @@ import signal
 import sys
 import hashlib
 import psutil
-from datetime import datetime
 
 
 DATA_DIR = '/ads_data'
@@ -31,7 +30,7 @@ def start_node(nconf_path, init=False, block_time=32):
     genesis['config'] = dict()
     genesis['config']['start_time'] = genesis_time
 
-    print("Genesis start time: ", datetime.strptime(str(genesis_time), '%s'))
+    print("Genesis start time: ", time.strftime("%Z - %Y/%m/%d, %H:%M:%S", float(genesis_time)))
 
     with open('genesis.json', 'w') as f:
         json.dump(genesis, f)
@@ -50,7 +49,7 @@ def start_node(nconf_path, init=False, block_time=32):
         os.kill(proc.pid, 0)
         with open('{0}.pid'.format(DAEMON_BIN_NAME), 'w') as f:
             f.write(str(proc.pid))
-        print("Process started: ", datetime.now())
+        print("Process started: ", time.strftime("%Z - %Y/%m/%d, %H:%M:%S", time.time()))
     except OSError:
         print("Server not started.")
         sys.exit(1)
