66db1c8cdaa6b719bbd06204e4f41173897a7580
sylwek <s.raczynski@bioinfo.com>
2018-06-14 16:07:49 +0200

    add showing hashes

--- src/tools/msgprinter.cpp
+++ src/tools/msgprinter.cpp
@@ -16,25 +16,24 @@ MsgPrinter::MsgPrinter(const std::string& filepath) : DataPrinter(filepath) {
 void MsgPrinter::printJson() {
     std::ifstream file;
     file.exceptions(std::ifstream::failbit | std::ifstream::badbit | std::ifstream::eofbit);
-    Header header;
+    Header      header;
+    EndHeader   endHeader;
 
     try {
         file.open(m_filepath, std::ifstream::in | std::ifstream::binary);
         file.read((char*)&header, sizeof(header));
 
-        uint32_t length  = header.type_and_length >> 8;
-        if (length < sizeof(header)) {
-            throw std::runtime_error("Bad length");
-        }
+        uint32_t length = header.lenght;
         length -= sizeof(header);
 
         uint8_t buffer[length];
         file.read((char*)&buffer, sizeof(buffer));
-        file.close();
+        file.read((char*)&endHeader, sizeof(EndHeader));
+
 
         boost::property_tree::ptree pt;
         // print header
-        pt.put("type", (header.type_and_length & 0xFF));
+        pt.put("type", (header.type));
         pt.put("signature", Helper::ed25519_key2text(header.signature, sizeof(header.signature))); // use key 2 text
         pt.put("svid", header.svid);
         pt.put("msid", header.msid);
@@ -58,7 +57,27 @@ void MsgPrinter::printJson() {
                 break;
             }
         }
-        pt.add_child("transactions", transactions);
+        pt.add_child("transactions", transactions);        
+
+        pt.put("hash", Helper::ed25519_key2text(endHeader.hash, sizeof(endHeader.hash))); // use key 2 text
+        pt.put("mnum", endHeader.mnum);
+        pt.put("tmax", endHeader.tmax);
+        pt.put("ttot", endHeader.ttot);
+
+        boost::property_tree::ptree hashvector;
+
+        for(int i=0;i<endHeader.tmax;i++)
+        {
+            hash_s hash;
+            file.read((char*)&hash, sizeof(hash_s));
+            boost::property_tree::ptree hashTree;
+            hashTree.put("", Helper::ed25519_key2text((uint8_t*)&hash, sizeof(hash)));
+            hashvector.push_back(std::make_pair("", hashTree)); // use key 2 text
+        }
+        pt.add_child("hashes", hashvector);
+
+        file.close();
+
         boost::property_tree::write_json(std::cout, pt, true);
         if (length > 0) {
             std::cout<<"Unsupported transaction, result might be not complete"<<std::endl;
