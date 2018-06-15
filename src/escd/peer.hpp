a69c9b2b68e416a99793270b76e6f47b91bea07b
Jacek Zemło <jacek@zel.pl>
2018-06-15 14:11:07 +0200

    Code cleanup
    (cherry picked from commit 9a7cf1b6e6a4279adc9754aa62a0e461cd3451bb)

--- src/escd/peer.hpp
+++ src/escd/peer.hpp
@@ -2102,6 +2102,12 @@ NEXTUSER:
     friend class PeerConnectManager;
 
   private:
+    // used in handle_read_bank to handle chunks
+    uint16_t last_bank=0;
+    uint16_t last_msid=0;
+    int64_t weight=0;
+    uint64_t csum[4]= {0,0,0,0};
+
     uint32_t    svid; // svid of peer
     int         do_sync; // needed by server::get_more_headers , FIXME, remove this, user peer_hs.do_sync
     //bool        killme; // kill process initiated
--- src/escd/server.hpp
+++ src/escd/server.hpp
@@ -2870,6 +2870,7 @@ NEXTUSER:
                     auto lu=changes.find(luser); // get user
                     if(lu==changes.end()) {
                         user_t u;
+                        bzero(&u, sizeof(user_t));
                         lseek(fd,luser*sizeof(user_t),SEEK_SET); // should return '0s' for new user, ok for xor4
                         read(fd,&u,sizeof(user_t));
                         changes[luser]=u;
@@ -2924,6 +2925,7 @@ NEXTUSER:
             auto au=changes.find(utxs.auser); // get user
             if(au==changes.end()) {
                 user_t u;
+                bzero(&u, sizeof(user_t));
                 lseek(fd,utxs.auser*sizeof(user_t),SEEK_SET); // should return '0s' for new user, ok for xor4
                 read(fd,&u,sizeof(user_t));
                 changes[utxs.auser]=u;
