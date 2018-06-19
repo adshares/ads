360ec4f69afcfbf3c3bb4810be939943e3940949
sylwek <s.raczynski@bioinfo.com>
2018-06-19 11:20:35 +0200

    Merge with picostock. https://github.com/picostocks/esc/commit/c18ef1d37b718f883091be265c8ef27b6ccebfcf?diff=unified

--- src/escd/server.hpp
+++ src/escd/server.hpp
@@ -1124,8 +1124,7 @@ NEXTUSER:
         check_msgs_.clear();
         check_.unlock();
 
-        //clean and undo txs messages
-        std::deque<message_ptr> signnew; // vector of own late message for time update, signature and resubmission
+        //clean and undo txs messages        
         uint16_t lastsvid=0;
         uint32_t minmsid=0;
         if(!txs_msgs_.empty()) {
@@ -1172,7 +1171,7 @@ NEXTUSER:
                         remove_message(tm->second);
                         txs_msgs_.erase(tm);
                         if(msg->svid==opts_.svid) {
-                            signnew.push_front(msg);
+                            sign_msgs_.push_front(msg);
                         }
                     } else {
                         ELOG("INVALIDATE message %04X:%08X [min:%08X len:%d]\n",tm->second->svid,tm->second->msid,minmsid,tm->second->len);
@@ -1192,7 +1191,7 @@ NEXTUSER:
                         remove_message(tm->second);
                         txs_msgs_.erase(tm);
                         if(msg->svid==opts_.svid) {
-                            signnew.push_front(msg);
+                            sign_msgs_.push_front(msg);
                         }
                     } else {
                         ELOG("MOVE message %04X:%08X [min:%08X len:%d]\n",tm->second->svid,tm->second->msid,minmsid,tm->second->len);
@@ -1253,28 +1252,7 @@ NEXTUSER:
                     missing_msgs_[tm->second->hash.num]=tm->second;
                 }
             }
-        }
-
-        if(!signnew.empty()) { // sign again messages that failed to be accepted by the network on time
-            uint32_t ntime=time(NULL);
-            uint32_t msid=srvs_.nodes[opts_.svid].msid;
-            hash_t msha;
-            memcpy(msha,srvs_.nodes[opts_.svid].msha,sizeof(hash_t));
-            for(auto mp=signnew.begin(); mp!=signnew.end(); mp++) {
-                msid++;
-                assert(msid==(*mp)->msid);
-                (*mp)->load(opts_.svid);
-                (*mp)->signnewtime(ntime,skey,pkey,msha);
-                (*mp)->status &= ~MSGSTAT_BAD;
-                memcpy(msha,(*mp)->sigh,sizeof(hash_t));
-                (*mp)->save();
-                (*mp)->unload(opts_.svid);
-                check_.lock(); //maybe not needed if no validators
-                check_msgs_.push_back((*mp));
-                check_.unlock();
-                ntime++;
-            }
-        }
+        }        
 
         //txs_msgs_ clean
         block_only=true; // allow validation of block messages only
@@ -1294,6 +1272,29 @@ NEXTUSER:
         }
         txs_.unlock();
         DLOG("LAST_block_final finished\n");
+    }
+
+    void signlater()
+    { if(!sign_msgs_.empty()){ // sign again messages that failed to be accepted by the network on time
+        uint32_t ntime=time(NULL);
+        uint32_t msid=srvs_.nodes[opts_.svid].msid;
+        hash_t msha;
+        memcpy(msha,srvs_.nodes[opts_.svid].msha,sizeof(hash_t));
+        for(auto mp=sign_msgs_.begin();mp!=sign_msgs_.end();mp++){
+          msid++;
+          assert(msid==(*mp)->msid);
+          (*mp)->load(opts_.svid);
+          (*mp)->signnewtime(ntime,skey,pkey,msha); //FIXME, insert_user lacks data !
+          (*mp)->status &= ~MSGSTAT_BAD;
+          memcpy(msha,(*mp)->sigh,sizeof(hash_t));
+          (*mp)->save();
+          (*mp)->unload(opts_.svid);
+          check_.lock(); //maybe not needed if no validators
+          check_msgs_.push_back((*mp));
+          check_.unlock();
+          ntime++;}
+        sign_msgs_.clear();
+      }
     }
 
     void count_votes(uint32_t now,hash_s& cand) {
@@ -4360,6 +4361,7 @@ NEXTBANK:
         }
         DLOG("NEW BLOCK created\n");
         srvs_.clean_old(opts_.svid);
+        signlater(); // sign own removed messages
     }
 
     //message_ptr write_handshake(uint32_t ipv4,uint32_t port,uint16_t peer)
@@ -4988,6 +4990,7 @@ NEXTBANK:
     std::map<hash_s,candidate_ptr,hash_cmp> candidates_; // list of candidates, TODO should be map of message_ptr
     message_queue wait_msgs_;
     message_queue check_msgs_;
+    message_queue sign_msgs_;
     std::map<hash_s,message_ptr,hash_cmp> bad_msgs_;
     message_map missing_msgs_; //TODO, start using this, these are messages we still wait for
     message_map txs_msgs_; //_TXS messages (transactions)
