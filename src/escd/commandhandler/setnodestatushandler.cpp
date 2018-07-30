911732a968b8bab078615fca0894a7060ea19f17
Jacek Zemło <jacek@zel.pl>
2018-07-30 16:25:19 +0200

    Fix fee checking

--- src/escd/commandhandler/setnodestatushandler.cpp
+++ src/escd/commandhandler/setnodestatushandler.cpp
@@ -70,7 +70,7 @@ void SetNodeStatusHandler::onExecute() {
         }
         boost::asio::write(m_socket, response);
     } catch (std::exception& e) {
-        DLOG("Responding to client %08X error: %s\n", m_usera.user, e.what());
+        DLOG("Responding to client %08X error: %s\n", m_command->getUserId(), e.what());
     }
 }
 
