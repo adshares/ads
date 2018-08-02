89f63a595fedfc86ebbb5491fe17a7bab19cb15e
Jacek Zemło <jacek@zel.pl>
2018-08-02 15:55:01 +0200

    Allow to withdraw half of BANK_MIN_UMASS to prevent bad messages

--- src/escd/server.hpp
+++ src/escd/server.hpp
@@ -3257,11 +3257,11 @@ NEXTUSER:
                 return(false);
             }
 
-            if(deduct+fee+(utxs.auser?0:BANK_MIN_UMASS)>usera->weight &&
-                deduct+fee+(utxs.auser?0:BANK_MIN_UMASS)>usera->weight+(int64_t)local_dsu[utxs.auser].deposit){
+            if(deduct+fee+(utxs.auser?0:BANK_MIN_UMASS/2)>usera->weight &&
+                deduct+fee+(utxs.auser?0:BANK_MIN_UMASS/2)>usera->weight+(int64_t)local_dsu[utxs.auser].deposit){
                 //network accepts total withdrawal from users and bank owners (otherwise dividend fee may invalidate message included in the next block)
                 ELOG("ERROR: too low balance txs:%016lX+fee:%016lX+min:%016lX>now:%016lX\n",
-                     deduct,fee,(uint64_t)(utxs.auser?0:BANK_MIN_UMASS),usera->weight+(int64_t)local_dsu[utxs.auser].deposit);
+                     deduct,fee,(uint64_t)(utxs.auser?0:BANK_MIN_UMASS/2),usera->weight+(int64_t)local_dsu[utxs.auser].deposit);
                 close(fd);
                 return(false);
             }
